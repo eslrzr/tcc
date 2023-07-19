81937f3b37baa7cd572c41e626ac487fd95427f6
João Arthur <joaoarthurd.lm@gmail.com>
2023-07-19 20:04:49 -0300

    adminlte configuration + auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\AdministrationType;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        // Create multiple administration types
+        AdministrationType::create([
+            'name' => 'Administrador',
+        ]);
+        AdministrationType::create([
+            'name' => 'Contador',
+        ]);
+        User::factory(1)->create(['administration_type_id' => 1]);
     }
 }
